1c412022831c2e83d3c283da77a55fcf65d84b1b
hayuuna <144312023+hayuuna@users.noreply.github.com>
2024-04-17 23:48:35 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.js
+++ 백준/Bronze/9086. 문자열/문자열.js
@@ -3,6 +3,6 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const num = Number(input[0]);
 
 for (let i = 1; i <= num; i++) {
-  let last = input[i].length - 1;
-  console.log(input[i][0] + input[i][last]);
+  let lastNum = input[i].length - 1;
+  console.log(input[i][0] + input[i][lastNum]);
 }
